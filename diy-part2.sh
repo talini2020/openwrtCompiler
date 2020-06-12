0430fdbea39730e2a2a87ab452657a0f308febc1
hetykai <3378432+hetykai@users.noreply.github.com>
2020-06-12 18:54:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,4 +9,12 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.168.3/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.168.3/g' package/base-files/files/bin/config_generate
+
+git clone https://github.com/awesome-openwrt/luci-app-arpbind package/luci-app-arpbind
+git clone https://github.com/awesome-openwrt/luci-app-autoreboot package/luci-app-autoreboot
+#git clone https://github.com/goomadao/luci-app-ipv6_ddns package/luci-app-ipv6_ddns
+git clone https://github.com/awesome-openwrt/luci-app-ramfree package/luci-app-ramfree
+git clone https://github.com/rufengsuixing/luci-app-syncdial package/luci-app-syncdial
+git clone https://github.com/maxlicheng/luci-app-unblockmusic package/luci-app-unblockmusic
+#git clone https://github.com/brvphoenix/luci-app-wrtbwmon package/luci-app-wrtbwmon
