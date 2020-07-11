171b92a90617f19710088c158b365072b1407719
hetykai <3378432+hetykai@users.noreply.github.com>
2020-07-11 09:36:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,7 +15,7 @@
 #git clone https://github.com/awesome-openwrt/luci-app-autoreboot package/luci-app-autoreboot
 #git clone https://github.com/goomadao/luci-app-ipv6_ddns package/luci-app-ipv6_ddns
 #git clone https://github.com/awesome-openwrt/luci-app-ramfree package/luci-app-ramfree
-rm -rf package/lean/luci-app-syncdial
+#rm -rf package/lean/luci-app-syncdial
 git clone https://github.com/rufengsuixing/luci-app-syncdial package/luci-app-syncdial
 #git clone https://github.com/maxlicheng/luci-app-unblockmusic package/luci-app-unblockmusic
 #git clone https://github.com/brvphoenix/luci-app-wrtbwmon package/luci-app-wrtbwmon
